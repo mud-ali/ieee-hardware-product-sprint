0a7e268df0a9a4f68374f47720955f5b3513f140
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-11-07 12:59:01 -0500

    random sequence of buttons to follow

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
 #include <Arduino.h>
 
-int buttonOne = 27;
+const int buttons[] = {32,33,34,35};
+const String button_names[] = { "yellow", "red", "blue","green"};
+const int BUTTON_COUNT = 4;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(buttonOne, INPUT);
+  Serial.begin(115200);
+  for (int i=0;i<BUTTON_COUNT;i++) {
+    pinMode(buttons[i], INPUT);
+  }
+}
+
+int randomButtonIndex() {
+  return random(BUTTON_COUNT);
 }
 
 void loop() {
-  int value = digitalRead(buttonOne);
-  Serial.println(value);
-  delay(1000);
+  int current_button_index = randomButtonIndex();
+  Serial.println("press the "+button_names[current_button_index]+" button");
+  while (!digitalRead(buttons[current_button_index])) {
+    delay(100);
+  }
+  Serial.println("yay");
+  delay(300);
 }
